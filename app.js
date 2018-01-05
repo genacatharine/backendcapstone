d0723742f916130fffdcce136c92c2e24b3f13db
Gena Catherine <gena.catharine@gmail.com>
2018-01-05 11:14:34 -0700

    added cors

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-
+With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS,
+PATCH, DELETE')
+  next()
+})
+
 app.use('/', index);
 app.use('/users', users);
 
