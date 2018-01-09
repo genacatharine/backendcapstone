2de0fb46d1cf0ffecd313f8bba6293d5e1bd9f71
Gena Catherine <gena.catharine@gmail.com>
2018-01-09 15:46:07 -0700

    adding jwt

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,27 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
-const knex= require('../knex')
+var bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex')
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  knex('users')
-    .select(
-      'id',
-      'email'
-    )
-    .then(function(data){
-      res.send(data)
+router.post('/', (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then((hashed_password) => {
+      console.log(hashed_password)
+      return knex('users')
+        .insert({
+          email: req.body.email,
+          hashed_password: hashed_password
+        }, '*');
     })
-})
-
+    .then((users) => {
+      const user = users[0];
+      delete user.hashed_password;
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      next(err);
+    });
+  })
 
 module.exports = router;
