6129a6ffcce1b1f1f43f741f4b1dfcb018f249c7
Gena Catherine <gena.catharine@gmail.com>
2018-01-12 09:23:07 -0700

    200

--- routes/tohikelist.js
+++ routes/tohikelist.js
@@ -2,32 +2,18 @@ const knex = require('../knex');
 const express = require('express');
 const router = express.Router();
 
-const auth = (req, res, next) => {
-  console.log('cookies', req.cookies);
-  jwt.verify(req.cookies['mhj-jwt'], secret, (err, payload) => {
-    if (err) {
-      console.log('you hit your own error code dumbass', err);
-      res.sendStatus(401)
-    }
-    console.log('before', payload);
-    req.payload = payload
-    console.log("payload: >>>", payload)
-    next()
-  })
-}
-
-router.post('/:id', auth, (req, res, next) => {
+router.post('/:id', (req, res, next) => {
   // console.log(`>>> ${req.params.id}`);
+  res.send(200);
   let payloadid = req.payload.usersid
   knex('tohikelist')
   .insert({
-    user_id: payloadid,
     hike_id: req.body.hike_id
   }, '*')
   .then((data) => {
-    console.log('DATA backend', data)
     let newObj = {
-      'user_id': payloadid,
+      'id': data[0].id,
+      'user_id': data[0].user_id,
       'hike_id': data[0]
     }
     res.send(newObj)
