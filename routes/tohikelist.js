4e8e6f8b26f11a1cdbf1e146abeb69d9b9ce123b
Gena Catherine <gena.catharine@gmail.com>
2018-01-12 09:42:57 -0700

    add to hike list

--- routes/tohikelist.js
+++ routes/tohikelist.js
@@ -4,20 +4,25 @@ const router = express.Router();
 
 router.post('/:id', (req, res, next) => {
   // console.log(`>>> ${req.params.id}`);
-  res.send(200);
-  let payloadid = req.payload.usersid
+  // res.send(200);
+  console.log('HELLOOOOOOOOOOOOOOO')
+  let payloadid = req.body
+  console.log('payloadid', payloadid)
   knex('tohikelist')
   .insert({
-    hike_id: req.body.hike_id
+    hike_id: 2,
+    users_id: 1
   }, '*')
   .then((data) => {
     let newObj = {
       'id': data[0].id,
-      'user_id': data[0].user_id,
+      'users_id': data[0].users_id,
       'hike_id': data[0]
     }
+    console.log('about to send response')
     res.send(newObj)
   }).catch((err) => {
+    console.log('ERRORRRRR', err)
     next(err);
   });
 })
