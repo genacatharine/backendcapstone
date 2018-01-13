f2b5d34b209b7eaf59c1a622fbc3f5cecb702ee0
Gena Catherine <gena.catharine@gmail.com>
2018-01-13 10:32:38 -0700

    receivign from front end

--- routes/tohikelist.js
+++ routes/tohikelist.js
@@ -7,26 +7,30 @@ require('dotenv').config()
 const SECRET = process.env.JWT_KEY
 
 router.post('/:id', (req, res, next) => {
-  let user_id = req.payload.usersId
-  knex('tohikelist')
-  .insert({
-    user_id: decod,
-    hike_id: req.body.hike_id
-  }, '*')
-  .then((data) => {
-    let newObj = {
-      'id': data[0].id,
-      'user_id': data[0].user_id,
-      'hike_id': data[0]
-    }
-  //   console.log('about to send response')
-    res.send(newObj)
-  }).catch((err) => {
-    console.log('ERRORRRRR', err)
-    next(err);
-  });
+  
+  console.log('receving from front end', req.body.clientToken)
+  // let user_id = req.payload.usersId
+  // knex('tohikelist')
+  // .insert({
+  //   user_id: decod,
+  //   hike_id: req.body.hike_id
+  // }, '*')
+  // .then((data) => {
+  //   let newObj = {
+  //     'id': data[0].id,
+  //     'user_id': data[0].user_id,
+  //     'hike_id': data[0]
+  //   }
+  // //   console.log('about to send response')
+  //   res.send(newObj)
+  // }).catch((err) => {
+  //   console.log('ERRORRRRR', err)
+  //   next(err);
+  // });
 })
-
+router.get('/', (req, res, next)=>{
+  console.log('hello routerget')
+})
 
 
 module.exports = router;
